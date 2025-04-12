be7670b7d8e48b9bf1fee74c453a123a895b2bf1
mira <mkyshchak@gmail.com>
2025-04-13 01:47:57 +0300

    create-file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 def main():
-    # write your code here
-    pass
+    # Запитуємо користувача ввести ім'я файлу (без розширення)
+    filename = input("Enter name of the file: ").strip()
+    full_filename = filename + ".txt"
+
+    # Збираємо рядки в список, поки не буде введено "stop"
+    content_lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line.strip().lower() == "stop":
+            break
+        content_lines.append(line)
+
+    # Створюємо файл із заданим ім'ям та записуємо в нього вміст
+    with open(full_filename, "w", encoding="utf-8") as file:
+        for content in content_lines:
+            file.write(content + "\n")
+
+    # Виводимо результат
+    print(f"# File name: \"{full_filename}\"")
+    print("# File content:")
+    for content in content_lines:
+        print(f"# {content}")
 
 
 if __name__ == "__main__":
